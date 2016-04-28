481d853a2e5e7643dc65ded2d33acef2ce171533
dawnstar64 <elena.ariza@menloschool.org>
2016-04-28 09:22:02 -0700

    1st commit
    hi

---
+++ Conway Game of Life/GameScene.swift
@@ -0,0 +1,88 @@
+//
+//  GameScene.swift
+//  Conway Game of Life
+//
+//  Created by Elena Ariza on 3/11/16.
+//  Copyright (c) 2016 Elena Ariza. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var _tiles:[[SKSpriteNode]] = []
+    var _margin = 4
+    
+    let _gridWidth = 400
+    let _gridHeight = 300
+    let _numRows = 8
+    let _numCols = 10
+    let _gridLowerLeftCorner:CGPoint = CGPoint(x: 255, y: 300)
+    
+    func calculateTileSize()-> CGSize
+    {
+        let tileWidth = _gridWidth / _numCols - _margin
+        let tileHeight = _gridHeight / _numRows - _margin
+        return CGSize(width: tileWidth, height: tileHeight)
+    }
+    
+    func getTilePosition(row r:Int, column c:Int) -> CGPoint
+    {
+        let tileSize = calculateTileSize()
+        let x = Int(_gridLowerLeftCorner.x) + _margin + (c * (Int(tileSize.width) + _margin))
+        let y = Int(_gridLowerLeftCorner.y) + _margin + (r * (Int(tileSize.height) + _margin))
+        return CGPoint(x: x, y: y)
+    }
+    
+    override func didMoveToView(view: SKView) {
+//        /* Setup your scene here */
+        
+//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+//        myLabel.text = "Hello, World!";
+//        myLabel.fontSize = 65;
+//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+//        
+//        self.addChild(myLabel)
+        
+        // initialize the 2d array of tiles
+        let tileSize = calculateTileSize()
+        for r in 0..._numRows {
+            var tileRow:[SKSpriteNode] = []
+            for c in 0..._numCols {
+                let tile = SKSpriteNode(color: UIColor.blueColor(), size: CGSize(width: 50, height: 50))
+                tile.size = CGSize(width: tileSize.width, height: tileSize.height)
+                
+                tile.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+                tile.position = getTilePosition(row: r, column: c)
+                self.addChild(tile)
+                tileRow.append(tile)
+            }
+            _tiles.append(tileRow)
+        }
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+//        for touch in touches {
+//            let location = touch.locationInNode(self)
+//            
+//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//            
+//            sprite.xScale = 0.5
+//            sprite.yScale = 0.5
+//            sprite.position = location
+//            
+//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            
+//            sprite.runAction(SKAction.repeatActionForever(action))
+//            
+//            self.addChild(sprite)
+//        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
