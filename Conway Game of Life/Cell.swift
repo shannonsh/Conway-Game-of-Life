e2998fdf648de71ba244e3b8b9c2c41d92cf1830
RainingFlamebows <Shannon@iphone-5.menloschool.org>
2016-04-28 13:18:37 -0700

    added ownership of cell to Cell

--- Conway Game of Life/Cell.swift
+++ Conway Game of Life/Cell.swift
@@ -9,10 +9,17 @@
 import Foundation
 
 
+
 class Cell {
+    // possible states of the cell
+    let dead = 0;
+    let P1 = 1; // cell is owned by player 1
+    let P2 = 2; // cell is owned by player 2
+    
     var x: Int;
     var y: Int;
-    var isAlive: Bool;
+    var state: Int;
+    
     /*
     * x0: initial x location of cell
     * y0: initial y location of cell
@@ -20,7 +27,7 @@ class Cell {
     init(x0: Int, y0: Int) {
         x = x0;
         y = y0;
-        isAlive = false;
+        state = dead;
     }
     
 }
